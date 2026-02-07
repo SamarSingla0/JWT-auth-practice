50a45f9508e98c06fd30db8e660c50eb1f171574
SamarSingla0 <samarsingla04@gmail.com>
2026-02-07 22:00:36 +0530

    second day of practice

--- users/views.py
+++ users/views.py
@@ -1,7 +1,43 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-
-
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from .models import User
+from rest_framework.exceptions import AuthenticationFailed
+import jwt
+import datetime
 class Register(APIView):
     def post(self, request):
-        pass # Create your views here.
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        
+        
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+        
+        user = User.objects.filter(email=email).first()
+        
+        if user is None:
+            raise AuthenticationFailed('User not found!')
+        
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password!')
+        
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+        token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
+        
+        response = Response()
+        
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {'jwt': token}
+        
+        return Response
+    
